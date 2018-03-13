8a743b103efd8996379c89dca2c6a358172057ae
coffenbacher <charles.offenbacher@gmail.com>
2018-03-13 17:21:53 -0400

    Adding PORT env variable

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ app.get('/', async (req, res) => {
   const ical = await calendar()
   res.send(ical)
 })
-app.listen(3000, () => console.log('Serving ical on port 3000'))
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Serving ical on port 3000'),
+)
